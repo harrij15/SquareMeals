4045ba8a5b094199ed600fd2df340f104053b4b4
harrij15 <harrij15@rpi.edu>
2016-03-01 11:59:29 -0500

    Modified login process

--- app/src/main/java/sm/MainActivity.java
+++ app/src/main/java/sm/MainActivity.java
@@ -1,23 +1,12 @@
 package sm;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Point;
 import android.os.Bundle;
-import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
-import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Display;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-import android.widget.GridView;
-import android.widget.LinearLayout;
-import android.widget.PopupWindow;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -26,11 +15,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 
-    // Pushes on sign up view on click of sign up button
+    //Pushes on sign up view on click of sign up button
     public void signUp(View view) {
         Intent newIntent = new Intent(this,SignUpActivity.class);
         startActivity(newIntent);
+    }
+
+    //@zefanya: use this function to connect the activity to the
+    //homepage if the user/pass is filled (the button is already set up for you)
+    public void logIn(View view) {
+
+        EditText username = (EditText)findViewById(R.id.user_name);
+        EditText password = (EditText)findViewById(R.id.password);
+        TextView incorrect = (TextView)findViewById(R.id.incorrect);
+        // Logs in user only if the username/password fields are filled
+        if (username.getText().length() != 0 && password.getText().length() != 0) {    // If user/pass are both filled
+            //Intent homepageIntent = new Intent(this,homepage.class);  //Don't forget to change the class!
+            //startActivity(homepageIntent);
+
+        } else {    //Tell user to fill in correct information
+            incorrect.setVisibility(View.VISIBLE);
+        }
+
+
+    }
+
+    //@zefanya: use this function to connect the activity to the homepage (the button is already set up for you)
+    public void guestEnter(View view) {
+         /*
+              Intent homepageIntent = new Intent(this,homepage.class);  //Don't forget to change the class!
+              startActivity(homepageIntent);
+          */
     }
 }
