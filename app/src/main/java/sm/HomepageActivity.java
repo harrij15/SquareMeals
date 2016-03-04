5c731a74ba3e5f83d0d1ad3e0db22cdfee942d95
Zefanya Putri <zefanyanadia.putri@yahoo.com>
2016-03-04 16:10:15 -0500

    Edit layout.

--- app/src/main/java/sm/HomepageActivity.java
+++ app/src/main/java/sm/HomepageActivity.java
@@ -20,6 +20,10 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.Spinner;
+import android.widget.TabHost;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 /**
  * Created by putriz on 2/23/2016.
@@ -28,23 +32,70 @@ import android.widget.Spinner;
 
 public class HomepageActivity extends AppCompatActivity {
 
+    TabHost tabhost;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
 
+        // TOOLBAR
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+/*        toolbar.setTitle(null);
+        toolbar.setLogo(R.drawable.leaf);
+        toolbar.setLogoDescription("It is a leaf");*/
+
+        // TABS
+        TabHost host = (TabHost)findViewById(R.id.tabHost);
+        host.setup();
+
+            // Tab 1
+            TabHost.TabSpec spec = host.newTabSpec("Top Picks for You");
+            spec.setContent(R.id.tab1);
+            spec.setIndicator("Top Picks for You");
+            host.addTab(spec);
+
+            // Tab 2
+            spec = host.newTabSpec("My Cookbook");
+            spec.setContent(R.id.tab2);
+            spec.setIndicator("My Cookbook");
+            host.addTab(spec);
+
         /**
          * Creates a dropdown menu in the homepage
          */
-        Spinner dropdown = (Spinner) findViewById(R.id.spinner);
-        String[] dropdown_list = new String[]{"Hello User! Top picks for you:","Cookbook","Preferences","Settings"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, dropdown_list);
-        dropdown.setAdapter(adapter);
+/*        Spinner dropdown = (Spinner) findViewById(R.id.spinner);
+        String[] dropdown_list = new String[]{"*","Preferences","Settings"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, dropdown_list);
+        dropdown.setAdapter(adapter);*/
 
         /**
          * Creates a grid view of recipe recommendations
          */
-        GridView gridView = (GridView) findViewById(R.id.gridView2);
+        GridView gridView = (GridView) findViewById(R.id.homepage_tab1_gridView);
         gridView.setAdapter(new HomepageButtonAdapter(this));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_hompage, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
