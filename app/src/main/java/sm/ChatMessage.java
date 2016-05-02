8c00d801cc43ef4fc7cfbabe41561ee12020c81d
Shreya <shreyap@sdpsoftware.com>
2016-05-01 21:43:31 -0400

    Added some classes for the blog posts, not completely done yet so I have not linked them to the main activity

---
+++ app/src/main/java/sm/ChatMessage.java
@@ -0,0 +1,19 @@
+package sm;
+
+/**
+ * Created by patels13 on 5/1/2016.
+ */
+public class ChatMessage {
+    public boolean left;
+    public String message;
+
+
+    public ChatMessage(boolean left, String message){
+
+        super();
+        this.left=left;
+        this.message=message;
+
+
+    }
+}
